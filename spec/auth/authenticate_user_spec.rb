af499e1a91374ead20ecfb8d2336a8a03252f6a7
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-05-28 16:43:49 +0100

    Write negative edge case test for authenticate user

--- spec/auth/authenticate_user_spec.rb
+++ spec/auth/authenticate_user_spec.rb
@@ -3,12 +3,23 @@ require 'rails_helper'
 RSpec.describe AuthenticateUser do
   let(:user) { create(:user) }
   subject(:valid_request_object) { described_class.new(user.email, user.password) }
+  subject(:invalid_request_object) { described_class.new("wrong@email.com", "wrongpassword") }
 
   context 'with valid credentials' do
     it 'should return a valid user object' do
       result = valid_request_object.call
       expect(result[:user])
         .to eq(user)
+    end
+  end
+
+  context 'with invalid credentials' do
+    it 'should raise authentication error' do
+      result = 
+      expect { invalid_request_object.call }
+        .to raise_error(
+          ExceptionHandler::AuthenticationError, "Invalid Credentials"
+        )
     end
   end
 end 
