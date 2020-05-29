8560e91f6426fb651c2b5c8479c0b942e3651c29
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-05-29 02:05:03 +0100

    Handle cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost.com', 'https://eventweatherapp-client.herokuapp.com', 'https://eventweatherapp-client.herokuapp.com'
+    origins '*'
 
     resource '*',
       headers: :any,
